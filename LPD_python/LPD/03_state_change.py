dc5628d6632cdddf4d38d7b8d525ea228016fe10
Federico <gianoli.federico@gmail.com>
2025-03-31 08:55:50 +0200

    Update 03_state_change.py

--- LPD_python/LPD/03_state_change.py
+++ LPD_python/LPD/03_state_change.py
@@ -28,7 +28,7 @@ def state_change(obj2process, yearsBaseline=3, changeNclass=1, filename=""):
 
     print(f"Raster shape: {obj2process.shape}")
     start_year = 1999
-    selected_years = np.arange(2008, 2024)  # Anni dal 2008 al 2023
+    selected_years = np.arange(2004, 2019)  # Anni dal 2008 al 2023
     band_indices = selected_years - start_year  # Converti anni in indici (base 0)
 
     print(f"Selected band indices: {band_indices}")
@@ -69,8 +69,8 @@ def state_change(obj2process, yearsBaseline=3, changeNclass=1, filename=""):
 
 
 if __name__ == "__main__":
-    input_raster = "/home/gianofe/Documents/corrected/sumndvi_multiband_xxl.tif"
-    output_raster = "/home/gianofe/Documents/corrected/state_changexxl.tif"
+    input_raster = "/scratch/gianofe/SumNDVI_correction_rev4/NDVI_SUM_multiband_rev4.tif"
+    output_raster = "/scratch/gianofe/SumNDVI_correction_rev4/outputs/state_change_2008_2023.tif"
 
     print(f"Opening input raster: {input_raster}")
     with rasterio.open(input_raster) as src:
